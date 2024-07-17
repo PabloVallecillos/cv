dc9d41628284fe065f5485ed95f906be2fa9e3d8
PabloVallecillos <vallecillospablo@gmail.com>
2024-07-17 19:11:51 +0200

    feat: Added projects and testing deploy.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-	i18n: {
-		defaultLocale: "en",
-		locales: ["es", "en"],
-		routing: {
-			prefixDefaultLocale: true
-		}
-	}
+  i18n: {
+    defaultLocale: "en",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: true
+    }
+  },
+  integrations: [tailwind(), robotsTxt()]
 });
